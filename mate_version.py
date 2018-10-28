9d36a1284435aba3125b098620bfd029317af23f
Michał Wieluński <michal@landmaj.pl>
2018-10-28 19:59:04 +0100

    import gtk in a backward compatible way

---
+++ mate_version.py
@@ -0,0 +1,44 @@
+import re
+import subprocess
+import sys
+from collections import namedtuple
+
+MateVersion = namedtuple("MateVersion", ["major", "minor", "patch"])
+pattern = re.compile(
+    b"(?P<major>\d+)\."
+    b"(?P<minor>\d+)\."
+    b"(?P<patch>\d+)"
+)
+
+
+def get_mate_version():
+    """
+    Return namedtuple with major, minor and patch version of Mate
+    or None if Mate is not installed.
+    """
+
+    try:
+        mate_about_output = subprocess.check_output(
+            ("mate-about", "--version")
+        )
+    except FileNotFoundError:
+        return None
+    match = pattern.search(mate_about_output)
+    return (MateVersion(
+        major=int(match.group("major")),
+        minor=int(match.group("minor")),
+        patch=int(match.group("patch")),
+    ))
+
+
+def import_gtk():
+    import gi
+
+    version = get_mate_version()
+    if version and version.major <= 2 and version.minor < 18:
+        gi.require_version("Gtk", "2.0")
+    elif version:
+        gi.require_version("Gtk", "3.0")
+    else:
+        # TODO: add logging
+        sys.exit(1)
--- setup.py
+++ setup.py
@@ -6,32 +6,35 @@
 #* see README for more information
 
 
-"""applet showing i3 workspaces in MATE""";
-
-
-from distutils.core import setup;
-from distutils.command.install_data import install_data;
+from distutils.core import setup
+from distutils.command.install_data import install_data
 
 
 class InstallData(install_data):
-	def run(self):
-		install_data.run(self);
+    def run(self):
+        install_data.run(self)
 
 
 setup(
-	name = "mate-i3-applet",
-	version = "1.0.0",
-	maintainer = "Matt Greer",
-	maintainer_email = "matt.e.greer@gmail.com",
-	description = "MATE I3 Workspace applet",
-	license = "BSD",
-	scripts = [],
-	url = "https://github.com/city41/mate-i3-applet",
-	package_dir = {},
-	packages = [],
-	data_files=[('/usr/lib/mate-i3-applet', ['matei3applet.py', 'log.py', 'i3conn.py', 'i3ipc.py']),
-		('/usr/share/dbus-1/services', ['matei3applet.service']),
-		('/usr/share/mate-panel/applets', ['matei3applet.mate-panel-applet']),
-	],
-	cmdclass={'install_data': InstallData}
+    name="mate-i3-applet",
+    version="2.0.1",
+    description="MATE i3 Workspace Applet",
+    long_description="Applet for MATE Panel showing i3 workspaces and mode.",
+    license="BSD",
+    url="https://github.com/city41/mate-i3-applet",
+    author="Matt Greer",
+    author_email="matt.e.greer@gmail.com",
+
+    data_files=[
+        ('/usr/lib/mate-i3-applet', [
+            'matei3applet.py',
+            'log.py',
+            'i3conn.py',
+            'i3ipc.py',
+            'mate_version.py',
+        ]),
+        ('/usr/share/dbus-1/services', ['matei3applet.service']),
+        ('/usr/share/mate-panel/applets', ['matei3applet.mate-panel-applet']),
+    ],
+    cmdclass={'install_data': InstallData}
 )
